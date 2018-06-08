57cc7d22447b1265639ab7e68d3055662e6c7291
amira2566 <braemira2@icloud.com>
2018-06-08 17:03:22 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # dog.rb
+class Dog
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+  def name
+    @this_dogs_name
+  end
+end
+
+  fido=Dog.new
+  fido.name
+  snoopy=Dog.new
+  snoopy.name
+  lassie=Dog.new
+  lassie.name
+      
+      
+      
+      
+      
